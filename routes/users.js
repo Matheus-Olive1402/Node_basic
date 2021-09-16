f9addd4b173feff46912b6db6e7e83cb0975ccc7
matheus oliveira <matheus.olive1402@hotmail.com>
2021-09-16 16:03:43 -0300

    routa POST

--- routes/users.js
+++ routes/users.js
@@ -16,5 +16,25 @@ router.get('/', function(req, res, next) {
       }))
 });
 
-
+// POST criar um recurso
+router.post('/', function (req, res, next) {
+    const {
+        nome,
+        cpf
+    } = req.body;
+    model.clientes.create({
+            nome: nome,
+            cpf: cpf
+        })
+        .then(clientes => res.status(201).json({
+            error: false,
+            data: clientes,
+            message: 'Novo cliente adicionado com sucesso'
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+  });
 module.exports = router;
